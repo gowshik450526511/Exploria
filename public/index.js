8527c9193b81add65a73a8fc095590ee1d16790c
gowshik450526511 <66863804+gowshik450526511@users.noreply.github.com>
2022-03-07 21:48:22 +0530

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,13 @@
+// var cost = 0;
+// function addition(item1)
+// {
+//     var place = document.getElementById('place').innerHTML;
+//     var budget = document.getElementById('budget').innerHTML;
+//     var room_charges = document.getElementById('room').innerHTML;
+//     console.log("Tourist Place" + " " +place);
+//     console.log("budget" +"  "+budget);
+//     console.log("roomcharges" + "  "+room_charges);
+//     cost = parseInt(budget) + parseInt(room_charges);
+//     console.log("total_cost" + " "+cost);
+//
+// }
